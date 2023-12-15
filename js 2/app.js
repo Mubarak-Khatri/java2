9369de3352231e072ee8a3c0c51f0d6549a52f8e
Mubarak-Khatri <147994459+Mubarak-Khatri@users.noreply.github.com>
2023-12-15 22:42:01 +0500

    Add files via upload

---
+++ js 2/app.js
@@ -0,0 +1,49 @@
+alert("Hello World")
+
+var zaid = "Hello Js"
+document.write (zaid ,"<br>")
+
+var zaid1 = 12345
+document.write (zaid1 ,"<br>")
+
+// var 1person = 1233       ******* illegel*********
+
+var person1 = 1233    //   ******* legel*********
+
+
+var abc = 2+2
+
+document.write( "Math expressions Familiar operators" ,"<br>","<br>",abc,"<br>");
+
+var abc1 = 3*2
+
+document.write(abc1,"<br>");
+
+var abc2 = 20/2
+
+document.write(abc2,"<br>");
+
+var abc3 = 20-2
+
+document.write(abc3,"<br>","<br>");
+
+
+var acd = 3
+
+acd++
+
+document.write("Math expressions Unfamiliar operators" ,"<br>","<br>",acd,"<br>");
+
+
+
+var num = (2 + 8) * 4 + 6
+++num
+document.write(num,"<br>","<br>");
+
+
+
+var num2 = 1 + (5 * 4);
+document.write("Math expressions Eliminating ambiguity" ,"<br>","<br>",num2,"<br>");
+
+
+
